cb403942cc23bcf024ba72bbf1d1e7a08729967b
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2024-12-22 07:18:56 +0900

    Create prisma service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
+  await app.listen(
+    // process.env.PORT ??
+    8000,
+  );
 }
 bootstrap();
