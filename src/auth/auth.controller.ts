c320f25d15631a598dc526e1933bceb96ea86363
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2024-12-22 13:01:37 +0900

    create auth endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() dto: AuthDto): Promise<Msg> {
+    return this.authService.signUp(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(
+    @Body() dto: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Msg> {
+    const jwt = await this.authService.login(dto);
+    res.cookie('access_token', jwt.accessToken, {
+      httpOnly: true,
+      secure: false, // 後ほど修正
+      sameSite: 'none',
+      path: '/',
+    });
+
+    return { message: 'ok' };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('logout')
+  logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Msg {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      secure: false, // 後ほど修正
+      sameSite: 'none',
+      path: '/',
+    });
+
+    return { message: 'ok' };
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { AuthDto } from './dto/auth.dto';
+import { Jwt, Msg } from './interfaces/auth.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+
+  async signUp(dto: AuthDto): Promise<Msg> {
+    const hashed = await bcrypt.hash(dto.password, 12);
+    try {
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashedPassword: hashed,
+        },
+      });
+      return { message: 'ok' };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          return { message: 'This email is already taken' };
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async login(dto: AuthDto): Promise<Jwt> {
+    // なぜか then が必要
+    const user = await this.prisma.user
+      .findUnique({
+        where: {
+          email: dto.email,
+        },
+      })
+      .then((user) => user);
+
+    if (!user) throw new ForbiddenException('Email or password is incorrect');
+    const isPasswordValid = await bcrypt.compare(
+      dto.password,
+      user.hashedPassword,
+    );
+    if (!isPasswordValid)
+      throw new ForbiddenException('Email or password is incorrect');
+
+    return this.generateJwt(user.id, user.email);
+  }
+
+  async generateJwt(userid: number, email: string): Promise<Jwt> {
+    const payload = { sub: userid, email: email };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '5m',
+      secret: secret,
+    });
+
+    return { accessToken: token };
+  }
+}
